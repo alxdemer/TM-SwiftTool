9cb5cc2c40f904a5eac9744309e98ce0ce8382f8
Alex Demerjian (Student Employee) <axdrcc@ITS-ROBIN.ad.rit.edu>
2022-10-19 14:02:14 -0400

    Started working on system info.

---
+++ TM-SwiftTool/TM-SwiftTool/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TM-SwiftTool
+//
+//  Created by Alex Demerjian (Student Employee) on 10/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TM-SwiftTool/TM-SwiftTool/TM_SwiftToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  TM_SwiftToolApp.swift
+//  TM-SwiftTool
+//
+//  Created by Alex Demerjian (Student Employee) on 10/19/22.
+//
+
+import SwiftUI
+
+@main
+struct TM_SwiftToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
