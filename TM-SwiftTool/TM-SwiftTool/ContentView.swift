32a8929c15a9be86d1a544798905003e342b648a
Alex Demerjian (Student Employee) <axdrcc@ITS-ROBIN.ad.rit.edu>
2022-10-19 14:02:36 -0400

    Working on system info.

--- TM-SwiftTool/TM-SwiftTool/ContentView.swift
+++ TM-SwiftTool/TM-SwiftTool/ContentView.swift
@@ -8,12 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //get computer hostname
+    let currentHost = Host.current().localizedName ?? ""
+    let userName = NSUserName()
+    var systemVersion = ProcessInfo.processInfo.operatingSystemVersionString
+    //var systemType = ProcessInfo.processInfo.
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            HStack
+            {
+                Text("System Info:")
+                    .underline()
+                    .padding()
+                
+                Text("Hardware Info:")
+                    .underline()
+                    .padding()
+            }
+            .padding()
+            
+            HStack
+            {
+                VStack
+                {
+                    Text("Hostname: " + currentHost)
+                    Text("Username: " + userName)
+                    Text("MacOS: " + systemVersion)
+                    //Text("System Type: " + systemType)
+                }
+                VStack
+                {
+                    
+                }
+            }
+            
+            
+            Button()
+            {
+                
+            }
+            label:
+            {
+                Image(systemName: "play.circle.fill")
+                Text("Senior Review")
+                
+            }
+            
         }
         .padding()
     }
